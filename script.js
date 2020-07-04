65692eff7e8c4d2a706165753cdb6201884d7d7c
Elaine Shaw <57796267+elshaw82@users.noreply.github.com>
2020-07-04 16:26:00 +0100

    Simple Pantone colour cards

---
+++ script.js
@@ -0,0 +1,19 @@
+const colourCards = document.querySelectorAll(".card")
+
+const body = document.querySelector("body");
+
+function gray(){
+    document.body.classList.toggle('gray');
+}
+
+function pink(){
+    document.body.classList.toggle('pink');
+}
+
+function blue(){
+    document.body.classList.toggle('blue');
+}
+
+function yellow(){
+    document.body.classList.toggle('yellow');
+}
